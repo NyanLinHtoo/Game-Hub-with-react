fe88f2febdf147bc44d7449d0d347370ad4b8b7b
NyanLinHtoo <nyanlinhtoo.ygn@gmail.com>
2024-06-21 21:04:37 +0700

    refactoring game grids

--- src/components/GameCards/GameGrid.tsx
+++ src/components/GameCards/GameGrid.tsx
@@ -12,24 +12,23 @@ const GameGrid = ({ gameQuery }: Props) => {
   const { data, errors, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+  if (errors) return <span>{errors}</span>;
+
   return (
-    <>
-      {errors && <span>{errors}</span>}
-      <ul className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-5 mx-3">
-        {isLoading &&
-          skeletons.map((skeleton) => (
-            <GameCardContainer key={skeleton}>
-              <GameCardsSkeletons />
-            </GameCardContainer>
-          ))}
-        {!isLoading &&
-          data.map((game) => (
-            <GameCardContainer key={game.id}>
-              <GameCards game={game} />
-            </GameCardContainer>
-          ))}
-      </ul>
-    </>
+    <ul className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-5 mx-3">
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <GameCardContainer key={skeleton}>
+            <GameCardsSkeletons />
+          </GameCardContainer>
+        ))}
+      {!isLoading &&
+        data.map((game) => (
+          <GameCardContainer key={game.id}>
+            <GameCards game={game} />
+          </GameCardContainer>
+        ))}
+    </ul>
   );
 };
 
